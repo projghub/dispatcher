aeb4c61da0d58f56bd952860cda100997574876e
Dane Jorgensen <dane@bgbng.com>
2012-10-15 20:53:37 -0600

    added api key

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Dispatch::Application.routes.draw do
   root :to => 'static_pages#index'
+
+  match 'api/customers' => 'api/customers#index'
+  match 'api/customers/create' => 'api/customers#create'
 end
